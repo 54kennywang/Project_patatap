f1498d53feb4aa9ba19d09b4b1ae9dcbbba2f309
kenny <qibowang7@outlook.com>
2019-04-18 21:40:55 -0400

    need to refactor js code into .js file

---
+++ Patatap/patatap.js
@@ -0,0 +1,7 @@
+// function onKeyDown(event) {
+//     var randomPoint = Point.random();
+//     var maxPoint = new Point(view.size.width, view.size.height);
+//     var point = randomPoint * maxPoint;
+//     var circle = new Path.Circle(point, 10);
+//     circle.fillColor = 'red';
+//   }
